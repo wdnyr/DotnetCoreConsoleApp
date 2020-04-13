92e97242d0ba2ea1702de42aac7f0c6e9d6dd8b7
Wim Denayer <wim.denayer@outlook.com>
2020-04-13 14:46:55 +0200

    Add project files.

---
+++ DotnetCoreConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+
+namespace DotnetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation("Logging information.");
+            logger.LogCritical("Logging critical information.");
+            logger.LogDebug("Logging debug information.");
+            logger.LogError("Logging error information.");
+            logger.LogTrace("Logging trace");
+            logger.LogWarning("Logging warning.");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
